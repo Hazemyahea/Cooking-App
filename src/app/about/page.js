5c02b39a026013cb644453000e1c7fb47cbf12de
Hazemyahea <hazemyahea1@gmail.com>
2023-11-27 01:39:11 +0200

    o

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -3,7 +3,7 @@ export default function AboutPage() {
     <div className=" container mx-auto">
       <h1 className="text-4xl text-center font-bold my-8">About Page</h1>
       <p className="text-center text-lg">
-        Hello , it's built with next js + Talwind css +{" "}
+        Hello , it's built with next js + Talwind css +
         <a href="https://www.themealdb.com/">API</a> it's crached sometimes
         especially in SingleFood page it's a simple next js project for learning
         purpose To Contact with me
