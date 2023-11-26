f3248cb4f916ab30d5a82d7c97d4b7ff64bc56cd
Hazemyahea <hazemyahea1@gmail.com>
2023-11-27 01:46:53 +0200

    l

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,23 +1,5 @@
 import React from "react";
 
 export default function AboutPage() {
-  return (
-    <div className="container mx-auto">
-      <h1 className="text-4xl text-center font-bold my-8">About Page</h1>
-      <p className="text-center text-lg">
-        Hello, it's built with Next.js + Tailwind CSS +
-        <a href="https://www.themealdb.com/">API</a>. It occasionally crashes,
-        especially on the SingleFood page. It's a simple Next.js project for
-        learning purposes. To contact me,
-        <a
-          href="https://twitter.com/HazemYdev"
-          className="text-blue-500"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          @HazemYdev
-        </a>
-      </p>
-    </div>
-  );
+  return <h1>about page</h1>;
 }
